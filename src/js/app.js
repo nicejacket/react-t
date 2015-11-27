2a869538de47add49898dd65410d4ff98080b3d7
zhou_xg <zhou84034339@sina.com>
2015-11-27 14:52:02 +0800

    add eslint dependency

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * @authors Your Name (you@example.org)
+ * @date    2015-11-26 16:36:12
+ * @version $Id$
+ */
+import '../sass/hello.scss';
+import React from 'react';
+
+const App = React.createClass({
+    render() {
+        return (
+          <div>
+            <h1>App</h1>
+            <ul></ul>{this.props.children}</div>
+        );
+    }
+});
+
+export default App;
