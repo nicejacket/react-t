54b7b5756446fed594c2befc42a8aaa3f14410da
nicejacket <zhou84034339@sina.com>
2015-11-28 22:03:55 +0800

    add test

--- karma.conf.js
+++ karma.conf.js
@@ -9,38 +9,43 @@ var webpack = require('webpack'),
 
 webpackConfig.devtool = 'inline-source-map';
 
-module.exports = function (config) {
-  config.set({
-    browsers: [ 'Chrome'], //run in Chrome
-    frameworks: ['mocha', 'chai'], //use the mocha test framework
-    files: [
-      'test/test.bundle.js' //just load this file
-    ],
-    preprocessors: {
-      'test/test.bundle.js': [ 'webpack', 'sourcemap' ]
-    },
-    reporters: [ 'mocha', 'coverage'], //report results in this format
-    coverageReporter: {
-      type: 'html',
-      dir: 'build/coverage/'
-    },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      noInfo: true
-    },
-    singleRun: true,
-    port: 9876,
-    colors: true,
-    plugins: [
-      require("mocha"),
-      require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
-      require('karma-chai'),
-      require('karma-mocha'),
-      require('karma-coverage'),
-      require('karma-sourcemap-loader'),
-      require('karma-webpack'),
-      require('karma-mocha-reporter')
-    ]
-  });
+module.exports = function(config) {
+    config.set({
+        browsers: ['Chrome'], //run in Chrome
+        frameworks: ['mocha', 'chai'], //use the mocha test framework
+        files: [
+            'test/test.bundle.js' //just load this file
+        ],
+        preprocessors: {
+            'test/test.bundle.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['mocha', 'coverage'], //report results in this format
+        mochaReporter: {
+            utput: 'autowatch'
+        },
+        coverageReporter: {
+            dir: 'build/coverage/',
+            type: 'html'
+        },
+        webpack: webpackConfig,
+        webpackMiddleware: {
+            noInfo: true
+        },
+        singleRun: true,
+        port: 9876,
+        colors: true,
+        plugins: [
+            require('mocha'),
+            require('phantomjs'),
+            require('karma-chrome-launcher'),
+            require('karma-phantomjs-launcher'),
+            require('karma-chai'),
+            require('karma-mocha'),
+            require('karma-coverage'),
+            require('karma-spec-reporter'),
+            require('karma-mocha-reporter'),
+            require('karma-sourcemap-loader'),
+            require('karma-webpack')
+        ]
+    });
 };
