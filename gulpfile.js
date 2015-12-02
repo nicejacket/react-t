5f3d465581e7ae623d9f81f3b4cd528e59fb9785
zhou_xg <zhou84034339@sina.com>
2015-12-02 14:43:58 +0800

    add ie8 fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
-
+var es3ify = require('gulp-es3ify');
+ 
 gulp.task('copy', function() {
-    return gulp.src(['src/index.html']
-    ).pipe(gulp.dest('build'));
+    return gulp.src([
+	    	'src/index.html',
+	    	'node_modules/html5shiv/src/html5shiv.js',
+	    	'node_modules/html5shiv/src/html5shiv-printshiv.js',
+	    	'node_modules/es5-shim/es5-shim.js',
+	    	'node_modules/es5-shim/es5-sham.js'
+    	]).pipe(gulp.dest('build'));
+});
+gulp.task('es3ify', function () {
+    return gulp.src('build/*.js')
+        .pipe(es3ify())
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['copy']);
+gulp.task('default', ['copy', 'es3ify']);
