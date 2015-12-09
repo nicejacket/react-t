17b51e63b5af3983ba12f242e582319ddacea653
zhou_xg <zhou84034339@sina.com>
2015-12-09 16:21:40 +0800

    no comment

--- karma.conf.js
+++ karma.conf.js
@@ -12,32 +12,53 @@ webpackConfig.devtool = 'inline-source-map';
 module.exports = function(config) {
     config.set({
         browsers: ['Chrome'], //run in Chrome
-        frameworks: ['mocha', 'chai'], //use the mocha test framework
+
+        frameworks: ['mocha', 'sinon-chai'], //use the mocha test framework
+
         files: [
             'test/test.bundle.js' //just load this file
         ],
+
         preprocessors: {
             'test/test.bundle.js': ['webpack', 'sourcemap']
         },
+
         reporters: ['mocha', 'coverage'], //report results in this format
+
         mochaReporter: {
             output: 'autowatch'
         },
+
         coverageReporter: {
             dir: 'build/coverage/',
-            type: 'html'
+            reporters: [{
+                type: 'html'
+            }, {
+                type: 'lcovonly'
+            }]
         },
+
         webpack: webpackConfig,
+
         webpackMiddleware: {
             noInfo: true
         },
+
+        logLevel: config.LOG_INFO,
+
         singleRun: true,
+
         port: 9876,
+
         colors: true,
+
         plugins: [
             require('mocha'),
+            require('sinon'),
+            require('sinon-chai'),
             require('karma-chrome-launcher'),
             require('karma-chai'),
+            require('karma-sinon-chai'),
             require('karma-mocha'),
             require('karma-coverage'),
             require('karma-mocha-reporter'),
