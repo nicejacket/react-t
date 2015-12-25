902fd3b6f4147389feb773b5c9be1667268691c4
zhou_xg <zhou84034339@sina.com>
2015-12-25 14:06:37 +0800

    test sinon subt

---
+++ test/tesla/components/Fetch-test.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @authors zhou_xg (zhou84034339@sina.com)
+ * @date    2015-12-24 09:55:45
+ * @version 1.0.0
+ */
+
+describe('fetch api test', () => {
+	afterEach(() => {
+		fetchMock.restore();
+	})
+
+	it('测试fetch api get接口', (done) => {
+		let obj = {fetch: fetch};
+		let stub = sinon.stub(obj, 'fetch');
+		stub.withArgs("http://www.tesla.com").returns(new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve({message: 'ok'});
+			}, 1);
+		}));
+
+		obj.fetch("http://www.tesla.com").then(function(response) {
+			expect(response.message).to.eql('ok2');
+			done();
+		});
+	})
+});
